3b784089c5c43591d1a2831ad030b1c0c5c9ba3f
Shubha1108 <shubhamp6929@gmail.com>
2025-04-08 11:04:16 +0530

    task added

--- passportseva/src/com/xworkz/passportapp/constants/Area.java
+++
@@ -1,5 +0,0 @@
-package com.xworkz.passportapp.constants;
-
-public enum Area {
-    Banglore,Hyderabad,Chennai,Mysore,Shimogga;
-}
--- passportseva/src/com/xworkz/passportapp/constants/Location.java
+++
@@ -1,6 +0,0 @@
-package com.xworkz.passportapp.constants;
-
-public enum Location {
-    CPVDelhi,postoffice;
-
-}
